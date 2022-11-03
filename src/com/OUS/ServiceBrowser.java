7cc8a1a9a23d8dab3fc0a7af72c8d968c792edc3
Denis <denis.selitsky@gmail.com>
2022-11-03 16:37:52 +0300

    feat: add work DiceService through rmi

--- src/com/OUS/ServiceBrowser.java
+++ src/com/OUS/ServiceBrowser.java
@@ -8,6 +8,7 @@ import java.rmi.*;
 import java.awt.event.*;
 import java.rmi.registry.*;
 
+
 public class ServiceBrowser {
     JPanel mainPanel;
     JComboBox serviceList;
@@ -15,17 +16,19 @@ public class ServiceBrowser {
 
     public void buildGUI() {
         JFrame frame = new JFrame("RMI Browser");
+        frame.pack();
         mainPanel = new JPanel();
-        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
-
+        mainPanel.setLayout(new BorderLayout());
         Object[] services = getServiceList();
 
         serviceList = new JComboBox<Object>(services);
-
         frame.getContentPane().add(BorderLayout.NORTH, serviceList);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        //frame.pack();
         serviceList.addActionListener(new MyListListener());
-        frame.setSize(500,500);
+        frame.setSize(550,550);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     void loadService(Object serviceSelection) {
@@ -33,7 +36,7 @@ public class ServiceBrowser {
             Service svc = server.getService(serviceSelection);
             mainPanel.removeAll();
             mainPanel.add(svc.getGuiPanel());
-            mainPanel.validate();
+            mainPanel.revalidate();
             mainPanel.repaint();
         }catch(Exception ex) {
             System.out.println("loadService error");
